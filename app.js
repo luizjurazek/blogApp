394c1aca498fe89f096ca7ce2be1bc1230de8b7a
Luiz Jurazek <luizjurazek@gmail.com>
2023-02-01 20:49:54 -0300

    Exibindo postagens, criando rotas de cadastro de usuário

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const session = require('express-session');
 const flash = require('connect-flash');
 require("./models/Postagem");
 const Postagem = mongoose.model("postagens");
+require("./models/Categoria");
+const Categoria = mongoose.model("categorias");
+const usuarios = require("./routes/usuario")
 
 // Configurações do app
     // Configurando sessão 
@@ -77,9 +80,39 @@ const Postagem = mongoose.model("postagens");
     })
 
 
+    app.get("/categorias", (req, res) => {
+        Categoria.find().lean().then((categorias) => {
+            res.render("categorias/index", {categorias: categorias});
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao renderizar as categorias!")
+            res.redirect("/")
+        })
+    })
+
+    app.get("/categorias/:slug", (req, res) => {
+        Categoria.findOne({slug: req.params.slug}).lean().then((categoria) => {
+            if(categoria){
+                Postagem.find({categoria: categoria._id}).lean().then((postagens) => {
+                    res.render("categorias/postagens", {postagens: postagens, categoria: categoria})
+                }).catch((err) => {
+                    req.flash("error_msg", "Houve um erro ao listar os posts!")
+                    res.redirect("/")
+                })
+
+            } else {
+                req.flash("error_msg", "Essa categoria não existe!")
+                res.redirect("/");
+            }
+
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao carregar a página da categoria");
+            res.redirect("/");
+        })
+    })
+
     //admin
     app.use('/admin', admin);
-
+    app.use('/usuarios', usuarios)
 
 // Outros
 const PORT = 8031
